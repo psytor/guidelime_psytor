cc5a2517d438de97daee48b91a2185c165049a48
psytor <psytor@gmail.com>
2021-09-05 09:18:07 +0200

    1-6_psytor_BE_Rogue
    This is the Original Commit for the BE Rogue

---
+++ 1-6_psytor_BE_Rogue.lua
@@ -0,0 +1,67 @@
+Guidelime.registerGuide(
+[[
+[D1-6 Blood Elf, Rogue - A guide created by *Psytor*]
+[GA Rogue,BloodElf]
+[N1-6 Psytor Blood Elf - Rogue Guide]
+
+All Credits goes to *Psytor* on Nethergarde Keep.
+Go to Magistrix Erona and Accept: [QA8325 Reclaiming Sunstrider Isle]
+Go to [G37.69,24.9Eversong Woods] and kill all the Mana Wyrms on your way that you need for[QC8325 Reclaiming Sunstrider Isle]
+Return to Magistrix Erona and Turn in [QT8325 Reclaiming Sunstrider Isle]
+Accept your class quest [QA9392 Rogue Training] and also [QA8326 Unfortunate Measures]
+Go sell your inventory you don't need at [G38.66,20.33Eversong Woods]
+Turn in your class quest [QT9392 Rogue Training] and accept [QA10071 Well Watcher Solanian]
+[T] Learn your Level 2 Skills
+Go to Well Watcher Solanian on the upper level turn in [QT10071 Well Watcher Solanian] and accept [QA8345 The Shrine of Dath'Remar] and [QA8330 Solanian's Belongings]
+Jump straight down to Arcanist Ithanas and accept [QA8336 Fistful of Slivers]
+Go through the building to Arcanist Helion and accept [QA8346 Thirst Unending]
+Equip the *Sunstrider Book Satchel* for 4 more slots.
+On your way to [G37.69,24.9Eversong Woods], *kill the Wyrm* and *Mana Tap the Wyrm*, find *the Solanian's Journal* here
+Now go to [G35.13,28.91Eversong Woods], *kill* and *tap* the *Wyrm* you still need, also kill Lynx on your way to find *the Solanian's Scrying Orb*
+On your way back finish the following [QC8346 Thirst Unending][QC8336 Fistful of Slivers][QC8326 Unfortunate Measures]
+Go to Arcanist Helion and Turn in: [QT8346 Thirst Unending]
+Go to Arcanist Ithanas and Turn in: [QT8336 Fistful of Slivers]
+Go to Magistrix Erona and Turn In: [QT8326 Unfortunate Measures] Accept: [QA8327Report to Lanthan Perilon]
+Make sure to equip best equipment and go sell your inventory you don't need at [G38.66,20.33Eversong Woods]
+[T] Learn your Level 4 Skills
+From now on, everytime you attack humanoids, try to *Pick Pocket* them
+Now go to [QT8327Report to Lanthan Perilon] and accept [QA8334 Aggression]
+[G31.33,22.74Eversong Woods] On your way to pick up the *Scroll of Scourge Magic* for [QC8330 Solanian's Belongings], *Kill the Tenders*.
+Now go to [QC8345 The Shrine of Dath'Remar] and also finish [QC8334 Aggression]
+Go back to Well Watcher Solanian and turn in: [QT8330 Solanian's Belongings] [QT8345 The Shrine of Dath'Remar]
+Go to Lanthan Perilon and turn in: [QT8334 Aggression] and accept [QA8335 Felendren the Banished]
+Follow the path on your Left to the Falthrien Academy and find [QC8335 Felendren the Banished] on the top floor. Kill everything on the way to complete:
+In your inventory you should find *Tainted Arcane Sliver* activate it to accept [QA8338 Tainted Arcane Sliver]
+[H] Hearthstone Back
+Go to Arcanist Helion turn in [QT8338 Tainted Arcane Sliver]
+Next go to Lanthan Perilon turn in [QT8335 Felendren the Banished] and Accept [QA8347 Aiding the Outrunners]
+Make your way to Outrunner Alarion turn in [QT8347 Aiding the Outrunners] and Accept [QA9704 Slain by the Wretched]
+Go to [QT9704 Slain by the Wretched] and accept the return quest [QA9705 Package Recovery]
+Now go back to Outrunner Alarion and turn in [QT9705 Package Recovery] and accept the following [QA8350 Completing the Delivery]
+Head to Falconwing Square to Innkeeper Delaniel set your [H] Hearthstone and Turn in [QT8350 Completing the Delivery]
+Make sure to equip best equipment and go sell your inventory you don't need at [G48.48,45.9Eversong Woods]
+Now accept all the following quests from Magister Rajonis [QA8472 Major Malfunction]
+From Aeldon Sunbrand [QA8463 Unstable Mana Crystals]
+From the Wanted posted [QA8468 Wanted: Thaelis the Hungerer]
+On your way to the [QC8468 Wanted: Thaelis the Hungerer] kill everything on the way and also find the *Unstable Mana Crystal*
+Make your way back to Falconwing Square complete [QC8472 Major Malfunction] [QC8463 Unstable Mana Crystals]
+Go back to Magister Rajonis turn in [QT8472 Major Malfunction] and accept [QA8895 Delivery to the North Sanctum]
+Then to Sergeant Kan'ren [QT8468 Wanted: Thaelis the Hungerer]
+Then Aeldon Sunbrand turn in [QT8463 Unstable Mana Crystals] and accept [QA9352 Darnassian Intrusions]
+Make sure to equip best equipment and go sell your inventory you don't need at [G48.48,45.9Eversong Woods]
+[T] Learn your Level 6 Skills
+
+]], "Psytor Leveling Guide")
+
+-- V 0.0.1
+-- N - Guide Name 
+-- D - Detailed description
+-- *text* - Highlighting
+-- GA - Affiliation or/and Class or/and Race
+-- NX - Set Next Guide to load when completed
+-- [QA### Title] Accept quest
+-- [QC### Title] Progress
+-- [QT### Title] Turn In
+-- [G ##.##,##.## Area] Location
+-- [H] Heartstone Sign
+-- [T] Visit Trainer
