e6c8a6f18456772b709b51f86da800ce5d892ae9
Psytor <psytor@gmail.com>
2021-09-17 10:10:54 +0200

    Signed-off-by: Psytor <psytor@gmail.com>

---
+++ 6-13_psytor_BE.lua
@@ -0,0 +1,131 @@
+Guidelime.registerGuide(
+[[
+[D6-13 Blood Elf, Rogue - A guide created by *Psytor*]
+[GA Rogue,BloodElf]
+[N6-13 Psytor Blood Elf - Rogue Guide]
+
+All Credits goes to *Psytor* on Nethergarde Keep.
+Head out of the Gate to Ley-Keeper Caidanis to Turn in [QT8895 Delivery to the North Sanctum] and accept the follow up [QA9119 Malfunction at the West Sanctum]
+Keep going to Apprentice Ralen and Accept [QA9035 Roadside Ambush]
+Continu on the main path to Apprentice Meledor Turn In [QT9035 Roadside Ambush] and accept [QA9062 Soaked Pages]
+On the other side and accross the bridge, underwater, Find the [QC9062 Soaked Pages]
+Go back to Apprentice Meledor to Turn In [QT9062 Soaked Pages] and accept [QA9064 Taking the Fall]
+Keep going on the path to Fairbreeze Village and meet with Velan Brightoak to start a [QA8491 Pelt Collection]
+Now go on the right side of the inn and find Ranger Degolien [G 43.32,70.85Eversong Woods] to accept the quest *Situation at Sunsail Anchorage* - The Pre-Quest is bugged so I cannot use the anchor for now
+Start killing *Springpaw* to start collecting *Springpat pelts*
+From Fairbreeze Village head to Saltheril's Haven to accept [QA9067 The Party Never Ends]
+Now on your way to find the *Darnassian Scouts* [G 36.78,61.02Eversong Woods]
+After killing the *Darnassian Scout* in your inventory look for *Incriminating Documents* to accept [QA8482 Incriminating Documents]
+Now head to Ley-Keeper Velana to Turn In [QT9352 Darnassian Intrusions] and [QT9119 Malfunction at the West Sanctum] and accept [QA8486 Arcane Instability]
+Now go around the Sanctum and complete [QC8486 Arcane Instability]
+Once completed go back to Ley-Keeper Velania [QT8486 Arcane Instability]
+[H] Hearthstone back to Falconwing Square
+Head to Aeldon Sunbrand and Turn In [QT8482 Incriminating Documents] and accept [QA8483 The Dwarven Spy]
+Go to Prospector Anvilward, [QC8483 The Dwarven Spy], talk to him, follow him inside, YOU SPY! FINISH HIM!!!
+Go back to Aeldon Sunbrand and Turn In [QT8483 The Dwarven Spy]
+Kill mobs to level [XP8]
+I would greatly suggest that at that point you learn Cooking with Quarelestra [G48.57,47.11 Eversong Woods] (2nd Floor)
+[V] Then go to Landraelanis [G49.01,46.98 Eversong Woods] and buy his *Recipe: Lynx Steak*
+Now start to think of your professions you may want - As we are heading for the first time in Silvermoon City
+[V] Make sure to equip best equipment and go sell your inventory you don't need at [G48.48,45.9Eversong Woods]
+Now head to Ranger Jaela to accept [QA8475 The Dead Scar]
+Go in the The Dead Scar and complete [QC8475 The Dead Scar]
+Go back to Ranger Jaela and Turn In [QT8475 The Dead Scar]
+Head to Instructor Antheol and Turn In [QT9064 Taking the Fall] and Accept the Follow Up [QA9066 Swift Discipline]
+Now head into Silvermoon and here are the important trainer I suggest to meet and train different professions
+First - First Aid go to Alestus [G 77.82,71.05Silvermoon City]- In the future ever cloth you get turn it in Bandages so you level up this profession
+Second - Fishing go to Drathen at [G 76.23,67.77Silvermoon City] (It may not seem that interesting but it will help your cooking later on)
+Go meet Olirea and buy yourself a nice Fishing Pole and some baits to help you with your fishing - From now on if you have the time and see a pod of fish - Fish it
+Now go to Vinemaster Suntouched [G79,58Silvermoon City]
+[V] Browse his goods and buy a *Suntouched Special Reserve*
+Now learn your professions - Choose wisely
+Let's go Discipline Apprentice Ralen [G45.23,56.46Eversong Woods]
+Target Apprentice Ralen and in your inventory use *Antheol's Disciplinary Rod* on him
+Now go meet with Hathvelion Sungaze to accept [QA 8884 Fish Heads, Fish Heads...] don't forget hit *Springpaws*
+Time to kill some Murloc and get their [QC8884 Fish Heads, Fish Heads...]
+Go back to Hathvelion Sungaze [QT8884 Fish Heads, Fish Heads...] and accept the Follow Up [QA8885 The Ring of Mmmrrrggglll]
+From here head to Velendris Whitemorn to accept [QA8480 Lost Armaments] and Captain Kalisendra and accept [QA8886 Grimscale Pirates!] On the way kill more *Springpaw* if needed
+Go through Sunsail Anchorage find some boxes with *Sindo'rei Armaments* Kill the *Wretched* around but don't spend all your time there - Go through in direction of the beach
+While looking for *Captain Kelisendra's Cargo* on the Ground find Mmmrrrggglll a big Orange Murloc [QC8885 The Ring of Mmmrrrggglll] & [QC8886 Grimscale Pirates!]
+In your inventory you should have [QA8887 Captain Kelisendra's Lost Rutters] otherwise kill more Murlocs
+Once again go back to Hathvelion Sungaze and Turn In [QT8885 The Ring of Mmmrrrggglll]
+Come back towards Sunsail Anchorage and finish [QC8480 Lost Armaments] and [QC8892 Situation at Sunsail Anchorage] Most armaments are underwater
+Time to Turn In [QT8886 Grimscale Pirates!] and [QT8887 Captain Kelisendra's Lost Rutters]
+Now Turn In [QT8480 Lost Armaments] and Accept the follow up [QA9076 Wretched Ringleader]
+Back to Sunsail Anchorage we'll climb to kill the [QC9076 Wretched Ringleader]
+Go back to Velendris Whitemorn and Turn In [QT9076 Wretched Ringleader]
+Let's finish to discipline the bad apprentice head to *Apprentice Meledor* [G44.87,61.03Eversong Woods] and Displine him with *Antheol's Disciplinary Rod* to complete [QC9066 Swift Discipline]
+Go to Fairbreeze Village and You should have the pelt by now for Turn In [QT8491 Pelt Collection]
+Now let's accept all the quests in the Village [QA9254 The Wayward Apprentice] [QA9258 The Scorched Grove] [QA9358 Ranger Sareyn]
+Go up and Turn In [QT8892 Situation at Sunsail Anchorage] and accept [QA9359 Farstrider Retreat]
+Find *Halis Dawnstrider* and browse his goods to by a *Bundle of Fireworks*
+Let's now head to Ranger Sareyn to Turn In [QT9358 Ranger Sareyn] and accept [QA9252 Defending Fairbreeze Village]
+From here we'll head down to Turn in [QT9066 Swift Discipline] on the way kill some *Rotlimb Marauder*
+You should now be [XP10], otherwise kill stuff until you are
+Since we are close to Silvermoon City let's get the *Rogue lvl 10* quest! Find *Zelanis* and Accept [QA9532 Find Keltus Darkleaf]
+Work on your professions, clear your inventory, etc...
+[S] Set your Heartstone in Silvermoon City
+From here let's head to [QT9359 Farstrider Retreat] and accept [QA8476 Amani Encroachment]
+Find *Zalene Firstlight*[G60.40,62.46 Eversong Woods] and browse her goods to buy *Springpaw Appetizer*
+Then accept [QA8477 The Spearcrafter's Hammer] and [QA8888 The Magister's Apprentice]
+Let's go back to The Dead Scar at the East Sanctum and Turn In [QT9254 The Wayward Apprentice] and Accept [QA8487 Corrupted Soil]
+Kill the Undead needed and get the soils [QC9252 Defending Fairbreeze Village] [QC8487 Corrupted Soil]
+Go back to Turn In to [QT8487 Corrupted Soil]
+The next quest from *Apprentice Mirveda* is a protect Quest, once accepted protect her.
+When ready accept [QA8488 Unexpected Results]
+Once they are dead Turn In [QT8488 Unexpected Results] and Accept the Follow Up [QA9255 Research Notes]
+Let's head in the direction of Fairbreeze to Turn In [QT9252 Defending Fairbreeze Village] and Accept Follow [QA9253 Runewarden Deryan]
+Then to Fairbreeze Village to Turn In [QT9255 Research Notes] and Accept [QA9144 Missing in the Ghostlands]
+Then let's go to the Saltheril's Estate to finally Turn In [QT9067 The Party Never Ends]
+Let's keep going to Larianna Riverwind Turn In [QT9258 The Scorched Grove] and accept [QA8473 A Somber Task]
+On you way to kill Green keepers find *Old Whitebark*[G35.11, 83.98 Eversong Woods] and loot his Pendant to start [QA8474 Old Whitebark's Pendant]
+Cross The Scorched Grove while killing *Green Keepers* and head to [QT9532 Find Keltus Darkleaf] and accept the Follow Up [QA9460 Combining Forces]
+Now go pickpocket Lacy's Handkerchief for [QC9460 Combining Forces]
+Return to Turn In [QT9460 Combining Forces] and accept follow up [QA9618 Return the Reports]
+Complete [QC8473 A Somber Task]
+Return and Turn In [QT8474 Old Whitebark's Pendant] and [QT8473 A Somber Task] accept Follow Up [QA10166 Whitebark's Memory]
+Let's go to the Rune Stone in The Scorched Grove [G37.22,86.39Eversong Woods]
+Now open your Inventory and Activate the *Old Whitebark's Pendant* and attack Withebark's Spirit until you can talk to him and Turn In [QT10166 Whitebark's Memory]
+Let's go visit the Runewarden Deryan and Turn In [QT9253 Runewarden Deryan] and Accept the Follow Up [QA8490 Powering our Defenses]
+Let's go [QC8490 Powering our Defenses] - This is a protect quest - Stay Alive until Completion
+Return to Runwarden Deryan and Turn In [QT8490 Powering our Defenses]
+Time to go back to [H] Silvermoon user your Hearthstone
+Go Turn in [QT9618 Return the Reports]
+[V] Sell your stuff - Work your professions and let's head back out when ready
+Let's now go to Apprentice Apprentice Loralthalis [QT8888 The Magister's Apprentice]
+Accept the quests from Apprentice Loralthalis [QA8889 Deactivating the Spire] [QA9394 Where's Wyllithen?]
+And now [QT9394 Where's Wyllithen?] Once you found him Accept [QA8894 Cleaning up the Grounds]
+Go to [G68.92,51.98 Eversong Woods] while killing stuff and use the *Orb of Translocation*
+Now go around and you start *Deactivating the Spires* Find the Magister's Journal and Accept [QA8891 Abandoned Investigations]
+Keep [QC8889 Deactivating the Spire]
+Make sure you completed [QC8894 Cleaning up the Grounds]
+Go back to Groundskeeper Wyllithen to Turn In [QT8894 Cleaning up the Grounds]
+Go back to Apprentice Loralthalis Turn In [QT8889 Deactivating the Spire] [QT9394 Where's Wyllithen?] Accept the Follow Up [QA8890 Word from the Spire]
+Head to Farstrider Retreat Turn In [QT8891 Abandoned Investigations] & [QT8890 Word from the Spire]
+Now let's go find Spearcrafter Otembe, then get [QC8477 The Spearcrafter's Hammer]
+From there head to the Zeb'Watha Find the prisoneer Ven'jashi and accept [QA8479 Zul'Marosh]
+Go to the *Building* at [G62.20,79.99 Eversong Woods] kill stuff getting there
+Climb the stairs up top and find *Chieftain Zul'Marosh* kill him to get his head and loot the *Amani Invasions Plans* to Start [QA9360 Amani Invasion]
+Go back to Ven'jashi and Turn In [QT8479 Zul'Marosh]
+Make sure [QC8476 Amani Encroachment] is completed
+Now let's head back to Farstrider Retreat and Turn In [QT9360 Amani Invasion] [QT8476 Amani Encroachment] [QT8477 The Spearcrafter's Hammer] and Accept [QA9363 Warning Fairbreeze Village]
+We are now going back to Fairbreeze Village to Turn In [QT9363 Warning Fairbreeze Village]
+Before you complete this guide make sure you have the quest *Missing in the Ghostlands* already
+]], "Psytor Leveling Guide")
+
+-- V 0.0.1
+-- N - Guide Name 
+-- D - Detailed description
+-- *text* - Highlighting
+-- GA - Affiliation or/and Class or/and Race
+-- NX - Set Next Guide to load when completed
+-- [QA### Title] Accept quest
+-- [QC### Title] Progress
+-- [QT### Title] Turn In
+-- [G ##.##,##.## Area] Location
+-- [H] Heartstone Sign
+-- [T] Visit Trainer
+-- [V] Vendor
+-- [P] Get New Flight Point
+-- [F] Fly to
+-- [S] Set Hearthstone
